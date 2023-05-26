044695b426ddf32d0e06fe840699140d087243fb
007fernando <124810673+007fernando@users.noreply.github.com>
2023-05-26 18:15:11 +0000

    creando una app

---
+++ first_project/hello/urls.py
@@ -0,0 +1,13 @@
+#importar una bliblioteca administradora de rutas
+from django.urls import path
+
+#importando vistas
+from . import views
+
+#declarandoo las rutas vistas
+urlpatterns = [
+    #get/hello/
+    path("", views.index, name="index"),
+    #get/hello/author
+    path("Author/", views.author, name="author"),
+    ] 
---
+++ first_project/hello/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+     # se importa HTTPResponse
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hola desde mi primera vista🌟")
+def author(request):
+    return HttpResponse("Author: Luis Fernado Palacios Villaseñor🌟")
